b490d9f47dfab83c7e1f72cc2bb0308d6eed61a9
Michael Pospisil <michaelcpospisil@gmail.com>
2015-02-17 00:33:48 -0800

    adding first complete for squad lab for submission

--- app.rb
+++ app.rb
@@ -69,6 +69,13 @@ get '/squads/:squad_id/edit' do
     erb :editsquad
 end
 #------------------------------------------------
+#------------------------------------------------
+#------------------------------------------------
+
+
+
+
+#------------------------------------------------
 #  STUDENT GET ROUTES ------
 #----------------- INDEX SQUAD OF STUDENTS ------
 get '/students' do
@@ -81,17 +88,43 @@ get '/students' do
     @students = students
     erb :indexstudents
 end
+
+#------------------------------------------
+#----------------- STUDENTS IN SQUAD ------
+get '/squads/:squad_id/students' do
+  students = []
+  id = params[:squad_id].to_i
+     @conn.exec("SELECT * FROM students WHERE squad_id=$1", [id]) do |result|
+      result.each do |student|
+        students << student
+      end
+    end
+    @students = students
+
+     id = params[:squad_id].to_i
+ 
+  squad = @conn.exec("SELECT * FROM squads WHERE id=$1", [id])
+  
+    @squad = squad[0]
+   
+    erb :studentsinsquad
+end
 #----------------- NEW STUDENT ------------------
 get '/squads/:squad_id/students/new' do
+   id = params[:squad_id].to_i
+  
+  squad = @conn.exec("SELECT * FROM squads WHERE id=$1", [id])
+     
+    @squad = squad[0]
   erb :newstudent
 end
 #----------------- SHOW STUDENT -----------------
 get '/squads/:squad_id/students/:student_id' do
-  # studentid = params[:id].to_i
+  id = params[:student_id].to_i
     
-  #   student = @conn.exec("SELECT * FROM students WHERE student_id=$1", [studentid])
+    student = @conn.exec("SELECT * FROM students WHERE student_id=$1", [id])
     
-  #   @student = student[0]
+    @student = student[0]
 
   erb :showstudent
 end
@@ -99,54 +132,100 @@ end
 
 #----------------- EDIT STUDENT -----------------
 get '/squads/:squad_id/students/:student_id/edit' do
+    id = params[:student_id].to_i
+  
+  student = @conn.exec("SELECT * FROM students WHERE student_id=$1", [id])
+     
+    @student = student[0]
 
+
+    id = params[:squad_id].to_i
+  
+  squad = @conn.exec("SELECT * FROM squads WHERE id=$1", [id])
+     
+    @squad = squad[0]
+
+  erb :editstudent
 end
+#------------------------------------------------
+#------------------------------------------------
+#------------------------------------------------
+
+
+
+
+ # ALTER TABLE students ADD CONSTRAINT squad_fk FOREIGN KEY (squad_Id) REFERENCES squads (id) ON DELETE NO ACTION;
+
+
 
 #------------------------------------------------
  # POST ROUTES -------'CREATE'-----------------------
+#------------------------------------------------
+#-------------------- SQUAD CREATE ----------------------------
 #------------------------------------------------
 post '/squads' do
   @conn.exec("INSERT INTO squads (name, mascot) VALUES ($1, $2)", [params[:name], params[:mascot]])
 
   redirect '/squads'
 end
-
+#------------------------------------------------
+#-------------------- STUDENT CREATE --------------------------
 #------------------------------------------------
 post '/squads/:squad_id/students' do
-  @conn.exec("INSERT INTO students (name, age, spirit_animal, squad_id) VALUES ($1, $2, $3, $4)", [params[:name], params[:age], params[:spirit_animal], params[:squad_id]])
-
-  redirect "/students"
+  id = params[:squad_id].to_i
+  # binding.pry
+  @conn.exec("INSERT INTO students (name, age, spirit_animal, squad_id) VALUES ($1, $2, $3, $4)", [params[:name], params[:age], params[:spirit_animal], id])
+  
+  redirect "/squads/#{id}"
 end
 
 #------------------------------------------------
 #   PUT ROUTES -----'UPDATE'--------------------------
 # -----------------------------------------------
+#--------------------- SQUAD UPDATE ---------------------------
+#------------------------------------------------
 put '/squads/:squad_id' do
   id = params[:squad_id].to_i
+  
   @conn.exec("UPDATE squads SET name = $1, mascot = $2 WHERE id = $3", [params[:name], params[:mascot], id])
   redirect '/squads'
 end
 
-# -----------------------------------------------
-put '/squads/:squad_id/students' do 
-
+#-----------------------------------------------
+#--------------------- STUDENT UPDATE -------------------------
+#------------------------------------------------
+put '/squads/:squad_id/students/:student_id' do 
+  id = params[:student_id].to_i
+  
+ 
+  squadid = params[:squad_id].to_i
+  
+  @conn.exec("UPDATE students SET name = $1, age = $2, spirit_animal = $3 WHERE student_id = $4", [params[:name], params[:age], params[:spirit_animal], id])
+  redirect "/squads/#{squadid}/students"
 end
 
 #------------------------------------------------
 # DELETE ROUTES ------'DESTROY'----------------------
 # -----------------------------------------------
+#------------------------------------------------
+#----------------------- SQUAD DESTROY -------------------------
+#------------------------------------------------
 delete '/squads/:squad_id' do
   id = params[:squad_id].to_i
   @conn.exec("DELETE FROM squads WHERE id = $1", [id])
   redirect '/squads' 
 end
-
-# -----------------------------------------------
-delete '/squads/:squad_id/students' do 
-
+#------------------------------------------------
+# --------------------- STUDENT DESTROY ------------------------
+#------------------------------------------------
+delete '/squads/:squad_id/students/:student_id' do
+  studentid = params[:student_id].to_i
+  @conn.exec("DELETE FROM students WHERE student_id = $1", [studentid]) 
+  id = params[:squad_id].to_i
+  redirect "/squads/#{id}/students"
+  
 end
-
-# -----------------------------------------------
+#------------------------------------------------
 
 
 
